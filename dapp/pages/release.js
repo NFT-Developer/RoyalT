a97d685b461969a2d0ba9b327654ed69681acfaf
Max Meier <max@studiomax.io>
2021-05-09 16:15:17 -0700

    create release outline

--- dapp/pages/release.js
+++ dapp/pages/release.js
@@ -1,6 +1,6 @@
 const walletMnemonic = process.env.MNEMONIC;
 import React, { Component } from "react";
-import { Grid, Container, Header } from "semantic-ui-react";
+import { Grid, Container, Header, Table} from "semantic-ui-react";
 import Layout from "../components/Layout";
 import { Link, Router } from "../routes";
 const Moralis = require("moralis");
@@ -16,7 +16,13 @@ if (!process.env.MORALIS_APP_ID) {
 }
 
 class Release extends Component {
-  state = { userAddress: "", username: "" };
+  constructor(props) {
+    super(props);
+    this.state = {
+      userAddress: "",
+      username: ""
+    };
+  }
 
   async componentDidMount() {
     this._isMounted = true;
@@ -41,24 +47,75 @@ class Release extends Component {
   render() {
     return (
       <Layout page="dashboard">
-        <Grid columns={6} style={{ marginTop: "10px" }}>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Grid.Row>
-                <h1
-                  style={{
-                    fontSize: "4em",
-                    fontWeight: "normal",
-                  }}
-                >
-                  Release
-                </h1>
-                <h2>Address:{this.state.userAddress}</h2>
-              </Grid.Row>
+        <Grid style={{ marginTop: "10px", paddingRight:"150px" }}>
+          <Grid.Row color="blue">
+            <Grid.Column>
+              Artist<br />Title
             </Grid.Column>
           </Grid.Row>
-          <Grid.Row>
+          <Grid.Row columns={2} color="red">
+            <Grid.Column color="pink">
+              Artwork
             <br />
+            Royalties Sold
+            <br />
+            [7 / 10]
+            </Grid.Column>
+            <Grid.Column>
+              <u>Royalty Holders:</u>
+              <br />Address....... %
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row color="green" style={{padding: "10px"}}>
+            <Table celled padded>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell singleLine>Release Title</Table.HeaderCell>
+                  <Table.HeaderCell>Minted Qty.</Table.HeaderCell>
+                  <Table.HeaderCell># Sold</Table.HeaderCell>
+                  <Table.HeaderCell>Base Cost</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell singleLine>
+                    Release Un    
+                  </Table.Cell>
+                  <Table.Cell>75</Table.Cell>
+                  <Table.Cell>
+                    1094
+                  </Table.Cell>
+                  <Table.Cell>
+                    0.001 ETH
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell singleLine>
+                    Release Un    
+                  </Table.Cell>
+                  <Table.Cell>75</Table.Cell>
+                  <Table.Cell>
+                    1094
+                  </Table.Cell>
+                  <Table.Cell>
+                    0.001 ETH
+                  </Table.Cell>
+                </Table.Row>  
+              </Table.Body>
+            </Table>
+          </Grid.Row>
+          <Grid.Row color="orange">
+            <Grid.Column>
+              + New Releases
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              Available:<br />Name
+            </Grid.Column>
+            <Grid.Column>
+              Cost<br />Release Date
+            </Grid.Column>
           </Grid.Row>
         </Grid>
       </Layout>
