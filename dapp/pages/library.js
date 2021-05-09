5d4e76fac9aee07cb0acb50b782474849707608e
Max Meier <max@studiomax.io>
2021-05-08 19:21:52 -0700

    drag and drop prototype

--- dapp/pages/library.js
+++ dapp/pages/library.js
@@ -12,7 +12,40 @@ import {
 } from "semantic-ui-react";
 import Layout from "../components/Layout";
 import { Link, Router } from "../routes";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 const Moralis = require("moralis");
+let songTokens = [
+  {
+    id: "1",
+    owner: "0x001",
+    title: "song 1",
+    artist: "a 1",
+    time: "12334",
+    liked: false,
+    artwork: "/images/test.jpg",
+    forSale: false,
+  },
+  {
+    id: "2",
+    owner: "0x002",
+    title: "song 2 - liked",
+    artist: "a 2",
+    time: "125346",
+    liked: true,
+    artwork: "/images/test.jpg",
+    forSale: false,
+  },
+  {
+    id: "3",
+    owner: "0x003",
+    title: "song 3 - for sale",
+    artist: "a 3",
+    time: "25436",
+    liked: false,
+    artwork: "/images/test.jpg",
+    forSale: true,
+  },
+];
 let appId;
 let serverURL;
 if (!process.env.MORALIS_APP_ID) {
@@ -47,6 +80,95 @@ class Library extends Component {
   componentWillUnmount() {
     this._isMounted = false;
   }
+  displayPlaylist2() {
+    return (
+      <DragDropContext>
+        <Droppable droppableId="playlist">
+          {(provided) => (
+            <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {songTokens.map(({ id, title, artist }, index) => {
+                return (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided) => (
+                      <li
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <p>{artist}</p>
+                      </li>
+                    )}
+                  </Draggable>
+                );
+              })}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }
+  displayPlaylist() {
+    return (
+      <DragDropContext>
+        <Droppable droppableId="songTokens">
+          {(provided) => (
+            <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {songTokens.map(
+                (
+                  { id, title, artist, time, liked, artwork, forSale },
+                  index
+                ) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <li
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <Table singleLine fixed>
+                            <Table.Body>
+                              <Table.Row>
+                                <Table.Cell style={{ width: "60px" }}>
+                                  <Icon size="big" name="play circle outline" />
+                                </Table.Cell>
+                                <Table.Cell>
+                                  <div class="plTitle">
+                                    <p>{title}</p>
+                                    <p>{artist}</p>
+                                  </div>
+                                </Table.Cell>
+                                <Table.Cell>
+                                  <p>{time}</p>
+                                </Table.Cell>
+                                <Table.Cell>
+                                  <p>Artwork...</p>
+                                </Table.Cell>
+                                <Table.Cell>
+                                  <Icon
+                                    size="large"
+                                    name={liked ? "heart outline" : "heart"}
+                                  />
+                                </Table.Cell>
+                                <Table.Cell>
+                                  <Icon name="ellipsis vertical" />
+                                </Table.Cell>
+                              </Table.Row>
+                            </Table.Body>
+                          </Table>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                }
+              )}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }
+
   render() {
     return (
       <Layout page="library">
@@ -76,58 +198,7 @@ class Library extends Component {
                     </Dropdown.Menu>
                   </Dropdown>
                 </Menu>
-                <Table singleLine>
-                  <Table.Body>
-                    <Table.Row>
-                      <Table.Cell>
-                        <Icon size="big" name="play circle outline" />
-                      </Table.Cell>
-                      <Table.Cell>
-                        <Icon name="add" />
-                      </Table.Cell>
-                      <Table.Cell>
-                        <p>Track1</p>
-                        <p>Artist</p>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <p>1:32</p>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <p>Artwork...</p>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <Icon size="large" name="heart outline" />
-                      </Table.Cell>
-                      <Table.Cell>
-                        <Icon name="ellipsis vertical" />
-                      </Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                      <Table.Cell>
-                        <Icon size="big" name="play circle outline" />
-                      </Table.Cell>
-                      <Table.Cell>
-                        <Icon name="add" />
-                      </Table.Cell>
-                      <Table.Cell>
-                        <p>Track2</p>
-                        <p>Artist</p>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <p>1:32</p>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <p>Artwork...</p>
-                      </Table.Cell>
-                      <Table.Cell>
-                        <Icon size="large" name="heart outline" />
-                      </Table.Cell>
-                      <Table.Cell>
-                        <Icon name="ellipsis vertical" />
-                      </Table.Cell>
-                    </Table.Row>
-                  </Table.Body>
-                </Table>
+                {this.displayPlaylist()}
               </div>
             </Grid.Column>
           </Grid.Row>
